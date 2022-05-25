e974c5a9663c3a05afeca7638a3f3125c24e95fd
ELDADBRHANO <ebrhano@gmail.com>
2022-05-25 11:32:04 +0300

    objects_second_page_commit

---
+++ main.js
@@ -0,0 +1,57 @@
+// var admin = {
+//   firstName: "eldad",
+//   lastName: "brhano",
+//   age:23,
+//   salary:8000
+// }
+
+
+// var worker ={
+//   fullName:"yonatan yitzhak",
+//   yearOfBirth:29/9/1997,
+//   email:"yon@gmail.com",
+//   addres:{
+//     city:"rishon le-tsion",
+//   }
+// }
+
+
+
+// var fruit ={
+//   name:"apple",
+//   color:"red",
+//   type:"solo"
+// }
+// document.write(fruit.name)
+// document.write(fruit.color)
+// document.write(fruit.type)
+
+
+
+
+
+// var kita={
+//   kitaMispar:4,
+//   numOfStudants:25,
+//   nameOfStudents:["eldad","yoni","sara","shlomo","lior"]
+// }
+// for(var i = 0;i<kita.nameOfStudents.length;i++){
+//   document.write(kita.nameOfStudents[i]+"<br>")
+// }
+
+
+
+// var kittensGardens={
+//   nameOfGardien:"anat",
+//   numOfChildren:25,
+//   nameOfChildren:["eldad","yoni","sara","shlomo","lior","roei","yuval","rei","rey","rami"]
+// }
+// for(var i=0; i< kittensGardens["nameOfChildren"].length;i++){
+//   document.write(kittensGardens["nameOfChildren"][i]+"<br>")
+// }
+// var myKey= prompt("enter key")
+// document.write(kittensGardens[myKey]);
+
+
+
+
